b3c36a211177139c2f08c890845889613e70afd5
setiya.a.wardoyo <setiya.a.wardoyo@gmail.com>
2024-12-20 11:59:23 +0000

    Ganti Ke Form Pendaftaran

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-[calc(100dvh-100px)] items-center justify-center gap-6">
-      <h2 className="text-3xl font-semibold">Games An-Naafi</h2>
+      <h2 className="text-3xl font-semibold">Link Pendaftaran</h2>
 
       <div className="flex flex-col w-full gap-3">
-        <Link href="/peta">
-          <Button className="w-full">Peta Ular Tangga</Button>
+        <Link href="https://forms.gle/4vRb33V6qJuPVUk59">
+          <Button className="w-full">Daftar Tahsin Anak</Button>
         </Link>
-        
-        <Link href="/">
-        <Button variant="outline" className="w-full border-primary border">Soal Ular Tangga</Button>
+
+        <Link href="https://forms.gle/MAQqy4yiKh9M3GPK8">
+          <Button variant="outline" className="w-full border-primary border">Daftar Tahsin Akselerasi</Button>
         </Link>
       </div>
     </div>
